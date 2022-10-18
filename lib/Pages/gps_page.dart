05081d55af38d0af907524ef84de09fc40d8f4c3
JhonySganzerla <jhonysganzerla@hotmail.com>
2022-10-18 20:52:47 -0300

    Adicionado Current position gps

--- lib/Pages/gps_page.dart
+++ lib/Pages/gps_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
@@ -13,6 +15,9 @@ class GpsPage extends StatefulWidget {
   class _GpsPageState extends State<GpsPage> {
     final _linhas = <String>[];
 
+    StreamSubscription<Position>?  _subscription;
+
+    bool get _monitorarLocalizacao => _subscription != null;
 
     @override
     void initState() {
@@ -23,7 +28,7 @@ class GpsPage extends StatefulWidget {
     Widget build(BuildContext context) {
       return Scaffold(
         appBar: AppBar(
-          title: Text('Usando GPS'),
+          title: const Text('Usando GPS'),
         ),
         body: _criarBody(),
       );
@@ -36,25 +41,30 @@ class GpsPage extends StatefulWidget {
             children: [
               ElevatedButton(
                   onPressed: _obterUltimaLocalizacao,
-                  child: Text('Obter a ultima localizacao conhecida (cache)')
+                  child: const Text('Obter a ultima localizacao conhecida (cache)')
               ),
               ElevatedButton(
                   onPressed: _obterLocalizacaoAtual,
-                  child: Text('Obter localização Atual')
+                  child: const Text('Obter localização Atual')
+              ),
+              ElevatedButton(
+                  onPressed: _monitorarLocalizacao ? _pararMonitoramento : _iniciarMonitoramento,
+                  child: Text(_monitorarLocalizacao ? 'Parar Monitoramento' : 'Iniciar Monitoramento')
               ),
               ElevatedButton(
                   onPressed: _limparLog,
-                  child: Text('Limpar Log')
+                  child: const Text('Limpar Log')
               ),
               const Divider(),
               Expanded(
-                  child:ListView.builder(
-                    shrinkWrap: true,
+                  child: ListView.builder(
+                      shrinkWrap: true,
                       itemCount: _linhas.length,
-                      itemBuilder: (_,index)=> Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: Text(_linhas[index]),
-                      )
+                      itemBuilder: (_, index) =>
+                          Padding(
+                            padding: const EdgeInsets.all(5),
+                            child: Text(_linhas[index]),
+                          )
                   ))
             ],
           ),
@@ -62,17 +72,18 @@ class GpsPage extends StatefulWidget {
 
     _obterUltimaLocalizacao() async {
       bool permissoesPermitidas = await _permissoesPermitidas();
-      if(!permissoesPermitidas){
+      if (!permissoesPermitidas) {
         return;
       }
 
       Position? position = await Geolocator.getLastKnownPosition();
-      
+
       setState(() {
-        if(position == null){
+        if (position == null) {
           _linhas.add('Nenhuma localização encontrada');
-        }else{
-          _linhas.add('Latitude: ${position.latitude} | Longitude: ${position.longitude} ');
+        } else {
+          _linhas.add('Latitude: ${position.latitude} | Longitude: ${position
+              .longitude} ');
         }
       });
     }
@@ -91,7 +102,8 @@ class GpsPage extends StatefulWidget {
       }
 
       if (permission == LocationPermission.deniedForever) {
-        _mostrarMensagem('Para utilizar esse recurso, você deverá acessar as configurações e permitir o GPs');
+        _mostrarMensagem(
+            'Para utilizar esse recurso, você deverá acessar as configurações e permitir o GPs');
         Geolocator.openAppSettings();
         return false;
       }
@@ -100,58 +112,75 @@ class GpsPage extends StatefulWidget {
 
     void _mostrarMensagem(String mensagem) {
       ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-              content: Text(mensagem)
-          ),
-        );
+        SnackBar(
+            content: Text(mensagem)
+        ),
+      );
     }
 
     Future<void> _showDialogMessage(String mensagem) async {
       await showDialog(
           context: context,
-          builder: (_) => AlertDialog(
-            title: const Text('Atenção'),
-            content: Text(mensagem),
-            actions: [
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () => Navigator.of(context).pop(),
+          builder: (_) =>
+              AlertDialog(
+                title: const Text('Atenção'),
+                content: Text(mensagem),
+                actions: [
+                  TextButton(
+                    child: const Text('OK'),
+                    onPressed: () => Navigator.of(context).pop(),
+                  )
+                ],
               )
-            ],
-          )
       );
     }
 
-    void _limparLog(){
+    void _limparLog() {
       setState(() {
         _linhas.clear();
       });
     }
 
     Future<bool> _servicoHabilitado() async {
-        bool servicoHabilitado = await Geolocator.isLocationServiceEnabled();
-        if(!servicoHabilitado){
-          await _showDialogMessage('Para utilizar este recurso você deve '
-              'habilitar o serviço de localização do dispositivo');
-          Geolocator.openLocationSettings();
-          return false;
-        }
-        return true;
+      bool servicoHabilitado = await Geolocator.isLocationServiceEnabled();
+      if (!servicoHabilitado) {
+        await _showDialogMessage('Para utilizar este recurso você deve '
+            'habilitar o serviço de localização do dispositivo');
+        Geolocator.openLocationSettings();
+        return false;
+      }
+      return true;
     }
 
-    void _obterLocalizacaoAtual() async{
+    void _obterLocalizacaoAtual() async {
       bool servicohabilitado = await _servicoHabilitado();
-      if(!servicohabilitado){
+      if (!servicohabilitado) {
         return;
       }
-       Position position = await Geolocator.getCurrentPosition();
+      Position position = await Geolocator.getCurrentPosition();
 
       setState(() {
+        _linhas.add('Latitude: ${position!.latitude} | Longitude: ${position!.longitude} ');
+      });
+    }
+
+    void _iniciarMonitoramento(){
+      _subscription = Geolocator.getPositionStream(
+        locationSettings: LocationSettings(
+          distanceFilter: 30,
+          timeLimit: Duration(seconds: 2),
+        ),
+      ).listen((Position position) {
+        setState(() {
           _linhas.add('Latitude: ${position!.latitude} | Longitude: ${position!.longitude} ');
-        }
-      );
+        });
+      });
 
     }
-}
+
+    void _pararMonitoramento(){
+      _subscription!.cancel();
+    }
+  }
 
 
